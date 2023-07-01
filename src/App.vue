1a0e66fc296258c29a551756e994c85b9a313519
chexq <chexq35@gmail.com>
2023-07-01 00:10:36 -0400

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// 这个配置项只做提示
-import HelloWorld from '@/components/HelloWorld.vue'
+
 </script>
 
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView />
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
