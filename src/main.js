37201e657cd9da9860c58f92f4d73a88996aac91
chexq <chexq35@gmail.com>
2023-07-03 11:45:47 -0400

    详情页全局组件插件化

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 // 引入初始化样式文件
 import '@/styles/common.scss'
-
 // 引入懒加载指令插件并注册
-import { lazyPlugin } from './directives'
-
+import { lazyPlugin } from '@/directives'
+// 引入全局组件插件
+import { componentPlugin } from '@/components'
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
