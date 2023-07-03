367b2b8a48719ec8566c25a1886400e2982aff41
chexq <chexq35@gmail.com>
2023-07-03 16:01:20 -0400

    登录基础业务实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref } from "vue"
+import { loginAPI } from '@/apis/user'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from "vue-router"
 // 表单校验（账户名+密码）
 // 1. 准备表单对象
 const form = ref({
-    account: '',
-    password: '',
+    account: '', // xiaotuxian001
+    password: '', // 123456
     agree: true
 })
 // 2. 主编内规则对象
@@ -32,13 +36,19 @@ const rules = {
 }
 // 3. 获取form实例做统一校验
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = () => {
+  const { account, password } = form.value
   // 调用实例方法
-  formRef.value.validate((valid) => {
+  formRef.value.validate(async (valid) => {
     // valid: 所有表单都通过校验 才为true
     // 以valid作为判断条件 如果通过校验才执行登录逻辑
     if (valid) {
-        //
+      const res = await loginAPI({ account, password })
+      // 1. 提示用户
+      ElMessage({ type: 'success', message: '登录成功' })
+      // 2. 跳转首页
+      router.replace({ path: '/' })
     }
   })
 }
@@ -46,7 +56,6 @@ const doLogin = () => {
 // 2. 同意协议 自定义规则 validator: (rule, value, callback) => {}
 // 3. 统一校验 通过调用form实例的方法 validate -> true
 </script>
-
 
 <template>
   <div>
