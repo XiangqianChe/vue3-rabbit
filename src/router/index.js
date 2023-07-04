54f1878ffa60088cb3524d6e33e38c7067f8b0f6
chexq <chexq35@gmail.com>
2023-07-04 18:16:18 -0400

    结算页提交订单业务实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 // createRouter: 创建router实例对象
 // createWebHistory: 创建history模式的路由
-
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
@@ -10,6 +9,7 @@ import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
+import Pay from '@/views/Pay/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // path和component对应关系的位置
@@ -41,6 +41,10 @@ const router = createRouter({
         {
           path: 'checkout',
           component: Checkout
+        },
+        {
+          path: 'pay',
+          component: Pay
         }
       ]
     },
